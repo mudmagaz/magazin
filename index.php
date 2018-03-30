473adc483bd756a273a40a29017aa8624ce89b75
Taras <tar_rudenko@ukr.net>
2018-03-30 08:57:40 +0300

    1commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once 'db.php';
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
+$template = $twig->loadTemplate('index.html');
+echo $template->render(array());
+
+
